7bc48a24ada5e8429b0b0ba3631816276b3eb2b8
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-12 13:04:29 -0300

    style: atualizados inputs da tela de perfil do usuario

--- src/app/features/autenticacao/login/login.component.ts
+++ src/app/features/autenticacao/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
@@ -10,6 +9,6 @@ import { InputTextModule } from 'primeng/inputtext';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [CommonModule, InputTextModule, InputNumberModule, ButtonModule, RouterLink],
+  imports: [CommonModule, InputTextModule, ButtonModule, RouterLink],
 })
 export class LoginComponent {}
--- src/app/features/usuarios/perfil/perfil.component.ts
+++ src/app/features/usuarios/perfil/perfil.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-perfil',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.scss']
+  styleUrls: ['./perfil.component.scss'],
+  standalone: true,
+  imports: [CommonModule, InputTextModule, ButtonModule, RouterLink],
 })
 export class PerfilComponent {
-
+  public usuario_autenticado = 'Giselle Barbosa';
 }
