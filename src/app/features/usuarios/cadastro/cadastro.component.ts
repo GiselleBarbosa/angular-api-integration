d088fdfba972e1e16cf04a7aae4ba8e3fed6c297
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-12 11:18:52 -0300

    style: iniciado formulario de cadastro

--- src/app/features/usuarios/cadastro/cadastro.component.ts
+++ src/app/features/usuarios/cadastro/cadastro.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InputTextModule, InputNumberModule, ButtonModule],
 })
 export class CadastroComponent {}
