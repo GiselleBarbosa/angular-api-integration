7d4b5d5043fbf1ce4c4d11c01aae6fe806219465
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-12 12:39:04 -0300

    style: adicionado formulario a tela de login

--- src/app/features/autenticacao/login/login.component.ts
+++ src/app/features/autenticacao/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InputTextModule, InputNumberModule, ButtonModule, RouterLink],
 })
 export class LoginComponent {}
--- src/app/features/usuarios/cadastro/cadastro.component.ts
+++ src/app/features/usuarios/cadastro/cadastro.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss'],
   standalone: true,
-  imports: [CommonModule, InputTextModule, InputNumberModule, ButtonModule],
+  imports: [CommonModule, InputTextModule, InputNumberModule, ButtonModule, RouterLink],
 })
 export class CadastroComponent {}
