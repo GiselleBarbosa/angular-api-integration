74b82663bce0fbb09c39910f61d930b9628089e1
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-13 21:26:19 -0300

    feat: dados atuais do usuario já sao populados automaticamente

--- src/app/core/services/api-usuarios.service.ts
+++ src/app/core/services/api-usuarios.service.ts
@@ -1,6 +1,15 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DestroyRef, inject, Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, shareReplay, tap, throwError } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  shareReplay,
+  Subject,
+  tap,
+  throwError,
+} from 'rxjs';
 import { Usuarios } from 'src/app/features/admin/interfaces/usuarios.interface';
 import { environment } from 'src/environments/environment';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
@@ -17,6 +26,9 @@ export class ApiUsuariosService {
 
   private errorSubject = new BehaviorSubject<string>('');
   public error$ = this.errorSubject.asObservable();
+
+  private usuarioCpfSubject = new Subject<Usuarios>();
+  public usuarioCpfSubject$ = this.usuarioCpfSubject.asObservable();
 
   private destroyRef = inject(DestroyRef);
 
@@ -45,5 +57,32 @@ export class ApiUsuariosService {
         takeUntilDestroyed(this.destroyRef)
       )
       .subscribe();
+  }
+
+  public listaUsuarioPorCPF(cpf: string): Observable<Usuarios> {
+    return this.http.get<Usuarios[]>(`${this.baseUrl}?cpf=${cpf}`, this.httpOptions).pipe(
+      map(usuarios => {
+        if (usuarios.length > 0) {
+          return usuarios[0]; // Retornando o primeiro usuário encontrado
+        } else {
+          throw new Error('Usuário não encontrado');
+        }
+      }),
+      tap(
+        usuario => this.usuarioCpfSubject.next(usuario),
+        () =>
+          this.errorSubject.next(
+            'Falha na requisição. Por favor, tente novamente mais tarde.'
+          )
+      ),
+      catchError(error => {
+        return throwError(
+          'Falha na requisição. Por favor, tente novamente mais tarde.',
+          error
+        );
+      }),
+      shareReplay(1),
+      takeUntilDestroyed(this.destroyRef)
+    );
   }
 }
--- src/app/features/admin/edicao/edicao.component.ts
+++ src/app/features/admin/edicao/edicao.component.ts
@@ -1,35 +1,88 @@
-import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { MessagesModule } from 'primeng/messages';
-import { PanelModule } from 'primeng/panel';
-import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { Subscription } from 'rxjs';
 import { ApiUsuariosService } from 'src/app/core/services/api-usuarios.service';
 
 @Component({
   selector: 'app-edicao',
-  standalone: true,
-  imports: [
-    PanelModule,
-    RouterLink,
-    TableModule,
-    ButtonModule,
-    NgClass,
-    NgIf,
-    NgFor,
-    FormsModule,
-    AsyncPipe,
-    MessagesModule,
-  ],
   templateUrl: './edicao.component.html',
   styleUrls: ['./edicao.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    AsyncPipe,
+    ReactiveFormsModule,
+    RouterLink,
+  ],
 })
-export class EdicaoComponent implements OnInit {
+export class EdicaoComponent implements OnInit, OnDestroy {
   private apiUsuariosService = inject(ApiUsuariosService);
+  private route = inject(ActivatedRoute);
+  private formBuilder = inject(FormBuilder);
+
+  public usuarioCpfSubject$ = this.apiUsuariosService.usuarioCpfSubject$;
+
+  public formularioAtualizacao!: FormGroup;
+
+  public cpf!: string;
+
+  private subcription!: Subscription;
 
   public ngOnInit(): void {
-    console.log('iniciado componente de edicao');
+    this.inicializarFormulario();
+
+    this.receberCpfDaRota();
+
+    this.popularDadosUsuarioSelecionado();
+  }
+
+  private receberCpfDaRota(): void {
+    this.subcription = this.route.paramMap.subscribe(params => {
+      const cpfRouta = params.get('cpf')!;
+      this.cpf = cpfRouta;
+    });
+  }
+
+  public inicializarFormulario(): void {
+    this.formularioAtualizacao = this.formBuilder.group({
+      cpf: [null],
+      nome: [null],
+      data_nascimento: [null],
+      telefone: [null],
+      email: [null],
+    });
+  }
+
+  public popularDadosUsuarioSelecionado(): void {
+    this.subcription = this.apiUsuariosService
+      .listaUsuarioPorCPF(this.cpf)
+      .subscribe(usuario => {
+        console.log(usuario);
+        this.formularioAtualizacao.patchValue({
+          cpf: usuario.cpf,
+          nome: usuario.nome,
+          data_nascimento: usuario.data_nascimento,
+          telefone: usuario.telefone,
+          email: usuario.email,
+        });
+      });
+  }
+
+  public atualizarDados(): void {
+    const dadosDoFormulario = this.formularioAtualizacao.getRawValue();
+
+    if (this.formularioAtualizacao.valid) {
+      console.log('enviar dados', dadosDoFormulario);
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.subcription.unsubscribe();
   }
 }
