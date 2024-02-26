39a25fbf5bff606bb44fc3503c6992c24a47efb3
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-26 16:53:04 -0300

    refac: adicionadas configuracoes de proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  baseUrl: 'http://localhost:3000/funcionarios',
-  apiBaseUrl: 'http://localhost:8080/funcionarios',
+  baseUrl: 'http://localhost:8080/api/v1/funcionarios',
   production: true,
 };
